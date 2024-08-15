78116aa72035799203aa6941b2548af3edfcfcb9
Axel Rindle <mail@axelrindle.de>
2024-08-15 15:02:35 +0200

    feat: use recommends and name rule

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,6 @@ import { configActDefault } from './lib/index.js'
 /** @type import('eslint').Linter.FlatConfig[] */
 const config = [
     ...configActDefault,
-    {
-        ignores: [
-            '*.d.ts',
-        ]
-    }
 ]
 
 export default config
